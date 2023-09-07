5f7d6304f710495cfad31a8bf0427b2ede9b613c
marrakechy <belmarrakechy21@coe.edu>
2023-09-06 22:06:16 -0500

    grade me

--- timing.py
+++ timing.py
@@ -1,3 +1,19 @@
 import time
 def timing(func):
     def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+
+        time_elapsed = start - end
+
+        print(f"{func.__name__} took {time_elapsed:.4f} seconds to execute")
+        return result
+
+    return wrapper
+
+    @timing
+    def random():
+        time.sleep(20)
+
+    random()
